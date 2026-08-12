e401517ada4394186c6ac4f3c2994a1efa328577
joomillet <josefina.millet@ug.uchile.cl>
2026-08-12 18:46:37 -0400

    agregue un metodo al archivo :v

--- usuario.py
+++ usuario.py
@@ -4,6 +4,11 @@ class Usuario:
         self.password = password
         self.email = email
         self.tareas = []
-
+    def listarTareas(self):
+        for tarea in self.tareas:
+            if tarea.estaLista():
+                print(f"[X] {tarea.obtenerNombre()}" )
+            else:
+                print(f"[ ] {tarea.obtenerNombre()}" )
     def agregarTarea(self, tarea):
         self.tareas.append(tarea)
